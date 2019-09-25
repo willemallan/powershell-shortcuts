8587aaa158edf525ce80d97da596c745518b1820
Willem Allan <willemarf@gmail.com>
2019-09-25 09:42:29 -0300

    update profile

--- Profile.ps1
+++ Profile.ps1
@@ -1,12 +1,21 @@
 # others
 function project {
     param (
-        [string] $msg
+        [string] $arg
     )
-    cd ~/projetos/$msg 
+    cd ~/projetos/$arg
+    code .
+}
+function dev {
+    cd ~/projetos/comissionamento
+    code .
 }
 
 # docker
+function test {
+    docker-compose run web pytest
+}
+
 function migrate {
     docker-compose run web python3 manage.py migrate
 }
@@ -15,8 +24,41 @@ function makemigrations {
     docker-compose run web python3 manage.py makemigrations
 }
 
-function rundocker {
+function loaddata {
+    param (
+        [string] $arg
+    )
+    cd ~/projetos/comissionamento/server
+    if ($arg) {
+        docker-compose run web python3 manage.py loaddata $arg
+    }
+    else {
+        docker-compose run web python3 manage.py loaddata state city
+    }
+}
+
+function permission {
+    param (
+        [string] $arg,
+        [string] $arg2
+    )
+    cd ~/projetos/comissionamento/server
+    if ($arg2) {
+        docker-compose run web python manage.py set_permission $arg --group $arg2
+    }
+    else {
+        docker-compose run web python manage.py set_permission $arg
+    }
+}
+
+function runback {
+    cd ~/projetos/comissionamento/server
     docker-compose up --build
+}
+
+function runfront {
+    cd ~/projetos/comissionamento/client
+    quasar dev
 }
 
 # git
@@ -26,27 +68,28 @@ function pull {
 
 function push {
     param (
-        [string] $msg
+        [string] $arg
     )
-    git push origin $msg
+    git push origin $arg
 }
 
 function cm {
     param (
-        [string] $msg
+        [string] $arg
     )
-    git commit -am $msg
+    git commit -am $arg
 }
 
 # git add all & commit
 function cma {
     param (
-        [string] $msg
+        [string] $arg
     )
     git add .
-    git commit -am $msg
+    git commit -am $arg
 }
 
 function s {
     git status
 }
+
